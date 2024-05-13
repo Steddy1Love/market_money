df9002df9398c75b8022dcb3091fe63652bcbced
Steddman Bell <steddfast@gmail.com>
2024-05-13 13:07:05 -0700

    Adds simplecov requirements for api endpoints in bin/rails and rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,10 @@ VCR.configure do |config|
   config.filter_sensitive_data('<API_KEY>') { Rails.application.credentials.tmdb[:key] }
   config.configure_rspec_metadata!
 end
+require 'simplecov'
+SimpleCov.start
+
+# Previous content of test helper now starts here
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
