7cba9b4fe411ac7ac1e3f9e7feb204b2766c3a6c
Steddman Bell <steddfast@gmail.com>
2024-05-13 21:29:45 -0700

    passing tests for us 1 & 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_03_30_155211) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_14_002114) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "market_vendors", force: :cascade do |t|
-    t.bigint "market_id"
-    t.bigint "vendor_id"
-    t.index ["market_id"], name: "index_market_vendors_on_market_id"
-    t.index ["vendor_id"], name: "index_market_vendors_on_vendor_id"
-  end
 
   create_table "markets", force: :cascade do |t|
     t.string "name"
     t.string "street"
     t.string "city"
     t.string "county"
-    t.string "state"
     t.string "zip"
     t.string "lat"
     t.string "lon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "vendors", force: :cascade do |t|
@@ -38,8 +32,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_03_30_155211) do
     t.string "contact_name"
     t.string "contact_phone"
     t.boolean "credit_accepted"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "market_vendors", "markets"
-  add_foreign_key "market_vendors", "vendors"
 end
