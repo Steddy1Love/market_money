33b74adc24fd120fc1944fc5214eac497b0a589d
lance butler <lancej2004@gmail.com>
2024-05-16 01:23:45 -0600

    US 5, Feat: creates a vendor(happy path)

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -10,4 +10,14 @@ class Api::V0::VendorsController < ApplicationController
       render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404)).serialize_json, status: :not_found
     end
   end
+
+  def create
+    render json: Vendor.create(vendor_params)
+  end
+
+  private
+
+  def vendor_params
+    params.require(:vendor).permit(:name, :description, :contact_name, :contact_phone, :credit_accepted)
+  end
 end
--- spec/requests/api/v0/vendors_request_spec.rb
+++ spec/requests/api/v0/vendors_request_spec.rb
@@ -1,32 +1,59 @@
 require "rails_helper"
 
 RSpec.describe "Vendors API" do
-  it "can get one vendor by its id" do
-    id = create(:vendor).id
+  describe "User Story 4" do
+    it "can get one vendor by its id" do
+      id = create(:vendor).id
 
-    get "/api/v0/vendors/#{id}"
+      get "/api/v0/vendors/#{id}"
 
-    vendor = JSON.parse(response.body, symbolize_names: true)
+      vendor = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    expect(vendor).to have_key(:id)
-    expect(vendor[:id]).to be_an(Integer)
+      expect(vendor).to have_key(:id)
+      expect(vendor[:id]).to be_an(Integer)
 
-    expect(vendor).to have_key(:name)
-    expect(vendor[:name]).to be_a(String)
+      expect(vendor).to have_key(:name)
+      expect(vendor[:name]).to be_a(String)
 
-    expect(vendor).to have_key(:description)
-    expect(vendor[:description]).to be_a(String)
+      expect(vendor).to have_key(:description)
+      expect(vendor[:description]).to be_a(String)
 
-    expect(vendor).to have_key(:contact_name)
-    expect(vendor[:contact_name]).to be_a(String)
+      expect(vendor).to have_key(:contact_name)
+      expect(vendor[:contact_name]).to be_a(String)
 
-    expect(vendor).to have_key(:contact_phone)
-    expect(vendor[:contact_phone]).to be_a(String)
+      expect(vendor).to have_key(:contact_phone)
+      expect(vendor[:contact_phone]).to be_a(String)
 
-    expect(vendor).to have_key(:credit_accepted)
-    expect([TrueClass, FalseClass]).to include(vendor[:credit_accepted].class)
+      expect(vendor).to have_key(:credit_accepted)
+      expect([TrueClass, FalseClass]).to include(vendor[:credit_accepted].class)
+    end
+  end
+
+  describe "User Story 5" do
+    it "can create a new vendor" do
+      vendor_params = ({
+                      name: 'Murder Inc.',
+                      description: 'Filled with suspense.',
+                      contact_name: 'Mira',
+                      contact_phone: '730-555-4435',
+                      credit_accepted: true
+                    })
+      headers = {"CONTENT_TYPE" => "application/json"}
+    
+      # We include this header to make sure that these params are passed as JSON rather than as plain text
+      post "/api/v0/vendors", headers: headers, params: JSON.generate(vendor: vendor_params)
+      created_vendor = Vendor.last
+
+      # binding.pry
+      expect(response).to be_successful
+      expect(created_vendor.name).to eq(vendor_params[:name])
+      expect(created_vendor.description).to eq(vendor_params[:description])
+      expect(created_vendor.contact_name).to eq(vendor_params[:contact_name])
+      expect(created_vendor.contact_phone).to eq(vendor_params[:contact_phone])
+      expect(created_vendor.credit_accepted).to eq(vendor_params[:credit_accepted])
+    end
   end
 
   describe 'sad paths' do
