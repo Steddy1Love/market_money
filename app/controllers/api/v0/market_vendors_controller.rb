cab9b52fffea05f7660f0559e21c64684533dc64
Steddman Bell <steddfast@gmail.com>
2024-05-16 22:34:27 -0700

    Save point 5-16

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -5,11 +5,15 @@ class Api::V0::MarketVendorsController < ApplicationController
     if params[:market_id].blank? || params[:vendor_id].blank?
       return render json: { error: "market_id and vendor_id are required" }, status: :bad_request
     end
-
+    
+    begin
     # Find the Market and Vendor
-    market = Market.find_by(id: params[:market_id])
-    vendor = Vendor.find_by(id: params[:vendor_id])
-
+    market = Market.find_by!(id: params[:market_id])
+    vendor = Vendor.find_by!(id: params[:vendor_id])
+    
+    rescue ActiveRecord::ValidationError => e
+      binding.pry
+      render json: ErrorSerializer.new(ErrorMessage.new(e.message)).serialize_json, status: :bad_request
     # Check if market or vendor does not exist
     if market.nil?
       return render json: { error: "Market not found" }, status: :not_found
@@ -29,7 +33,7 @@ class Api::V0::MarketVendorsController < ApplicationController
     market_vendor = MarketVendor.new(market: market, vendor: vendor)
 
     if market_vendor.save
-      render json: { message: "Vendor added to market" }, status: :created
+      render json: { message: "Successfully added vendor to market" }, status: :created
     else
       render json: { error: market_vendor.errors.full_messages.join(", ") }, status: :unprocessable_entity
     end
--- spec/requests/api/v0/market_vendors_request_spec.rb
+++ spec/requests/api/v0/market_vendors_request_spec.rb
@@ -2,8 +2,33 @@ require 'rails_helper'
 
 RSpec.describe "Api::V0::MarketVendors", type: :request do
   describe "Happy paths" do
-    it "Endpoint 8" do
-      
+    it "Endpoint 8 can create a new market vendor association" do
+      market1 = create(:market)
+      market2 = create(:market)
+      vendor1 = create(:vendor)
+      vendor2 = create(:vendor)
+      market_vendor_params = ({
+        market_id: market1.id,
+        vendor_id: vendor2.id
+      })
+      post "/api/v0/market_vendors", params: JSON.generate(market_vendor: market_vendor_params)
+      created_association = MarketVendor.last
+
+      expect(response).to be_successful
+      expect(created_association.market_id).to eq(market1.id)
+      expect(created_association.vendor_id).to eq(vendor2.id)
+    end
+  end
+
+  describe "Sad paths" do
+    xit "Endpoint 8 sad path 1" do
+      vendor = create(:vendor)
+      market_vendor_params = ({
+        market_id: 123123123,
+        vendor_id: vendor.id
+      })
+
+      post "/api/v0/market_vendors", params: JSON.generate
     end
   end
 end
